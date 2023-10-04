a5680a8e7a575dbb62cfe2d0ee2f794b8daa362e
Nathan Orr <norr@C02DQ4YUMD6R.peak6.net>
2023-10-04 11:19:46 +0100

    CVE Tester MVP

--- src/main/kotlin/utils/CVERequestParser.kt
+++ src/main/kotlin/utils/CVERequestParser.kt
@@ -7,19 +7,17 @@ fun parseMajorCVEs(filename: String): List<MajorCVE> {
     return File(filename).readLines()
         .drop(1) // Drop the header row
         .map { line ->
+            println("Processing line: $line") // Add this debug line
             val parts = line.split("|")
             MajorCVE(
                 cve = parts[0],
                 method = parts[1],
                 path = parts[2],
                 query_string = parts[3].takeIf { it.isNotBlank() },
-                headers = if (parts[4].isNotEmpty()) parts[4].let {
-                    it.substring(1, it.length - 1)
-                        .split(",")
-                        .associate {
-                            val (key, value) = it.split(": ")
-                            key.trim() to value.trim().removeSurrounding("\"")
-                        }
+                headers = if (parts[4].isNotEmpty()) {
+                    val pattern = Regex("\"([^\"]+)\":\\s\"([^\"]+)\"")
+                    val matches = pattern.findAll(parts[4])
+                    matches.associate { it.groupValues[1] to it.groupValues[2] }
                 } else emptyMap(),
                 post_data = parts[5].takeIf { it.isNotBlank() },
                 files = parts[6].takeIf { it.isNotBlank() },
